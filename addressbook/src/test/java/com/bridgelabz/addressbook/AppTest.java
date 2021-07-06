e5034d0bd8802efcbf1504c5e12b7c2ec2803027
PRADIP-1596 <9187pradipyadav@gmail.com>
2021-07-06 15:38:27 +0530

    [Pradip]:ADDED No Duplication Allowed And Enterd In AddressBook Problems

--- addressbook/src/test/java/com/bridgelabz/addressbook/AppTest.java
+++ addressbook/src/test/java/com/bridgelabz/addressbook/AppTest.java
@@ -5,10 +5,16 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+//import org.junit.Test;
 
-public class AppTest
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
 {
-   
+    /**
+     * Rigorous Test :-)
+     */
     @Test
     public void shouldAnswerWithTrue()
     {
