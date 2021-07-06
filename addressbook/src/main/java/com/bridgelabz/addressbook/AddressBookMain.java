ec86e479bd4054cd929ee6078e681940af25f78b
PRADIP-1596 <9187pradipyadav@gmail.com>
2021-07-06 14:04:28 +0530

    [Pradip]:ADDED Multiple Person In address book

--- addressbook/src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
+++ addressbook/src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -1,5 +1,13 @@
 package com.bridgelabz.addressbook;
 
-public class AddressBookMain {
+import java.util.Scanner;
+import java.lang.*;
 
+public class AddressBookMain {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to Address Book System ");
+		AddressBook addressBook = new AddressBook();
+		addressBook.getMenu();
+	}
 }
--- addressbook/src/main/java/com/bridgelabz/addressbook/Contacts.java
+++ addressbook/src/main/java/com/bridgelabz/addressbook/Contacts.java
@@ -9,45 +9,40 @@ public class Contacts {
     Long zipCode;
     Long phoneNumber;
     String email;
-    void Contactsdetails(){
-    	
-    }
 
- 
-
-	public Contacts() {
-		// TODO Auto-generated constructor stub
-	}
-
-
-
-
-
-	public String getFirstName() {
+    //getter and setter methods
+    
+    public String getFirstName() {
         return firstName;
     }
 
+    
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
     public String getLastName() {
+
         return lastName;
     }
 
     public void setLastName(String lastName) {
+
         this.lastName = lastName;
     }
 
     public String getAddress() {
+
         return address;
     }
 
     public void setAddress(String address) {
+
         this.address = address;
     }
 
     public String getCity() {
+
         return city;
     }
 
@@ -56,34 +51,42 @@ public class Contacts {
     }
 
     public String getState() {
+
         return state;
     }
 
     public void setState(String state) {
+
         this.state = state;
     }
 
     public Long getZipCode() {
+
         return zipCode;
     }
 
     public void setZipCode(Long zipCode) {
+
         this.zipCode = zipCode;
     }
 
     public Long getPhoneNumber() {
+
         return phoneNumber;
     }
 
     public void setPhoneNumber(Long phoneNumber) {
+
         this.phoneNumber = phoneNumber;
     }
 
     public String getEmail() {
+
         return email;
     }
 
     public void setEmail(String email) {
+
         this.email = email;
     }
 
