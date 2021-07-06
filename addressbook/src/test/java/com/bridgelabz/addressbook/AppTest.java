8ce95e932df563ea901e9d254043e720bb898067
PRADIP-1596 <9187pradipyadav@gmail.com>
2021-07-06 13:31:35 +0530

    [Pradip]:ADDED AddressBook Contact Details Mentioned A nd Excecute The Differ Branch

--- addressbook/src/test/java/com/bridgelabz/addressbook/AppTest.java
+++ addressbook/src/test/java/com/bridgelabz/addressbook/AppTest.java
@@ -1,17 +1,14 @@
 package com.bridgelabz.addressbook;
 
+
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+   
     @Test
     public void shouldAnswerWithTrue()
     {
